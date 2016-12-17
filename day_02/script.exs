577fb347fb998c9b581d547a9bec491d227fd1b0
Maciej Borowy <kontakt@mborowy.com>
2016-12-17 13:50:57 +0100

    add first part of day 2 exercise

--- day_02/script.exs
+++ day_02/script.exs
@@ -1,5 +1,34 @@
 defmodule Keypad do
   def to_code(instructions) do
-    
+    instructions
+    |> parse
+    |> find_sequence(5)
+    |> to_int
   end
+
+  defp parse(instructions) do
+    instructions
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
+  end
+
+  defp find_sequence(instructions, from) do
+    instructions
+    |> Enum.reduce([from], &make_sequence/2)
+    |> Enum.reverse
+    |> tl
+  end
+
+  defp make_sequence(moves, [from | _] = acc), do: [find_button(moves, from) | acc]
+
+  defp find_button(moves, from), do: moves |> Enum.reduce(from, &move/2)
+
+  defp move("U", from) when from in [4, 5, 6, 7, 8, 9], do: from - 3
+  defp move("D", from) when from in [1, 2, 3, 4, 5, 6], do: from + 3
+  defp move("L", from) when from in [2, 3, 5, 6, 8, 9], do: from - 1
+  defp move("R", from) when from in [1, 2, 4, 5, 7, 8], do: from + 1
+  defp move(_, from), do: from
+
+  defp to_int(sequence), do: sequence |> Enum.join |> String.to_integer
 end
--- day_02/tests.exs
+++ day_02/tests.exs
@@ -1,6 +1,71 @@
 Code.load_file("script.exs")
 ExUnit.start
 
+defmodule AcceptanceTest do
+  use ExUnit.Case
+
+  test "should convert given instructions to 1985" do
+    assert "ULL\nRRDDD\nLURDL\nUUUUD\n" |> Keypad.to_code == 1985
+  end
+end
+
 defmodule KeypadTest do
   use ExUnit.Case
+
+  setup do
+    %{instructions: "URDL\nUUUU\nDDDD\nLLLL"}
+  end
+
+  describe "to_code" do
+    test "instruction UUUU should return button 2" do
+      assert Keypad.to_code("UUUU") == 2
+    end
+
+    test "set up instruction should return 5287", %{instructions: instructions} do
+      assert instructions |> Keypad.to_code == 5287
+    end
+  end
+
+  # describe "parse" do
+  #   test "should split lines by new line", %{instructions: instructions} do
+  #     assert instructions |> Keypad.parse |> length == 4
+  #   end
+
+  #   test "each element should be list of moves to do", %{instructions: instructions} do
+  #     assert instructions |> Keypad.parse |> hd == ["U", "R", "D", "L"]
+  #   end
+  # end
+
+  # describe "move" do
+  #   test "one button up from button 5 is button 2" do
+  #     assert Keypad.move("U", 5) == 2
+  #   end
+
+  #   test "it is impossible to move one button up starting at any button in first row so should return same button" do
+  #     assert Keypad.move("U", 1) == 1
+  #     assert Keypad.move("U", 2) == 2
+  #     assert Keypad.move("U", 3) == 3
+  #   end
+
+  #   test "all impossible moves should return same button" do
+  #     assert Keypad.move("U", 1) == 1
+  #     assert Keypad.move("U", 2) == 2
+  #     assert Keypad.move("U", 3) == 3
+  #     assert Keypad.move("R", 3) == 3
+  #     assert Keypad.move("R", 6) == 6
+  #     assert Keypad.move("R", 9) == 9
+  #     assert Keypad.move("D", 9) == 9
+  #     assert Keypad.move("D", 8) == 8
+  #     assert Keypad.move("D", 7) == 7
+  #     assert Keypad.move("L", 7) == 7
+  #     assert Keypad.move("L", 4) == 4
+  #     assert Keypad.move("L", 1) == 1
+  #   end
+  # end
+
+  # describe "find_button" do
+  #   test "starting at button 5 and having instruction UUUU should return button 2" do
+  #     assert Keypad.find_button(["U", "U", "U", "U"], 5) == 2
+  #   end
+  # end
 end
