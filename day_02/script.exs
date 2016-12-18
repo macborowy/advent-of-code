0041c97caac7acc3927a2fabc8eaaa3301bb5e84
Maciej Borowy <kontakt@mborowy.com>
2016-12-18 18:22:06 +0100

    change module names and refactor up and down move/3 for :diamond

--- day_02/script.exs
+++ day_02/script.exs
@@ -1,7 +1,3 @@
-defmodule Keypad do
-  def to_code(instructions, shape), do: instructions |> Instruction.to_sequence(shape)
-end
-
 defmodule Instruction do
   def to_sequence(instructions, shape, from \\ "5") do
     instructions
@@ -26,32 +22,27 @@ defmodule Instruction do
 
   defp reduce_to_sequence(moves, [from | _] = acc, shape), do: [find_button(moves, from, shape) | acc]
 
-  defp find_button(moves, from, shape) do
-    moves
-    |> Enum.reduce(from, fn (m, acc) -> Move.move(m, acc, shape) end)
-  end
+  defp find_button(moves, from, shape),do: moves |> Enum.reduce(from, &(Keypad.move(&1, &2, shape)))
 end
 
-defmodule Move do
+defmodule Keypad do
   def move("U", from, :rectangle) when from in ["4", "5", "6", "7", "8", "9"], do: do_move(from, &(&1 - 3))
   def move("D", from, :rectangle) when from in ["1", "2", "3", "4", "5", "6"], do: do_move(from, &(&1 + 3))
   def move("L", from, :rectangle) when from in ["2", "3", "5", "6", "8", "9"], do: do_move(from, &(&1 - 1))
   def move("R", from, :rectangle) when from in ["1", "2", "4", "5", "7", "8"], do: do_move(from, &(&1 + 1))
 
   def move("L", from, :diamond) when from in ["3", "4", "6", "7", "8", "9", "B", "C"], do:
-  do_move(from, &(&1 - 1))
+    do_move(from, &(&1 - 1))
   def move("R", from, :diamond) when from in ["2", "3", "5", "6", "7", "8", "A", "B"], do:
-  do_move(from, &(&1 + 1))
+    do_move(from, &(&1 + 1))
 
-  def move("U", "3",  :diamond), do: "1"
+  def move("U", from, :diamond) when from in ["3", "D"], do: do_move(from, &(&1 - 2))
   def move("U", from, :diamond) when from in ["6", "7", "8"], do: do_move(from, &(&1 - 4))
   def move("U", from, :diamond) when from in ["A", "B", "C"], do: do_move(from, &(&1 - 4 - 7))
-  def move("U", "D",  :diamond), do: "B"
 
-  def move("D", "1",  :diamond), do: "3"
+  def move("D", from, :diamond) when from in ["1", "B"], do: do_move(from, &(&1 + 2))
   def move("D", from, :diamond) when from in ["2", "3", "4"], do: do_move(from, &(&1 + 4))
   def move("D", from, :diamond) when from in ["6", "7", "8"], do: do_move(from, &(&1 + 4 + 7))
-  def move("D", "B",  :diamond), do: "D"
 
   def move(_move, from, _keypad_shape), do: from
 
