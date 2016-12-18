2f07cd8189475e2cba7bcc469df48469d72e9133
Maciej Borowy <kontakt@mborowy.com>
2016-12-18 18:00:57 +0100

    Ugly buy working soultion to 2nd part of day 2

--- day_02/script.exs
+++ day_02/script.exs
@@ -1,15 +1,12 @@
 defmodule Keypad do
-  def to_code(instructions) do
-    instructions
-    |> Instruction.to_sequence(5)
-  end
+  def to_code(instructions, shape), do: instructions |> Instruction.to_sequence("5", shape)
 end
 
 defmodule Instruction do
-  def to_sequence(instructions, from) do
+  def to_sequence(instructions, from, shape) do
     instructions
     |> parse
-    |> find_sequence(from)
+    |> find_sequence(from, shape)
   end
 
   defp parse(instructions) do
@@ -19,23 +16,52 @@ defmodule Instruction do
     |> Enum.map(&String.graphemes/1)
   end
 
-  defp find_sequence(instructions, from) do
+  defp find_sequence(instructions, from, shape) do
     instructions
-    |> Enum.reduce([from], &reduce_to_sequence/2)
+    |> Enum.reduce([from], &(reduce_to_sequence(&1, &2, shape)))
     |> Enum.reverse
     |> tl
     |> Enum.join
   end
 
-  defp reduce_to_sequence(moves, [from | _] = acc), do: [find_button(moves, from) | acc]
+  defp reduce_to_sequence(moves, [from | _] = acc, shape), do: [find_button(moves, from, shape) | acc]
 
-  defp find_button(moves, from), do: moves |> Enum.reduce(from, &Move.move/2)
+  defp find_button(moves, from, shape) do
+    moves
+    |> Enum.reduce(from, fn (m, acc) -> Move.move(m, acc, shape) end)
+  end
 end
 
 defmodule Move do
-  def move("U", from) when from in [4, 5, 6, 7, 8, 9], do: from - 3
-  def move("D", from) when from in [1, 2, 3, 4, 5, 6], do: from + 3
-  def move("L", from) when from in [2, 3, 5, 6, 8, 9], do: from - 1
-  def move("R", from) when from in [1, 2, 4, 5, 7, 8], do: from + 1
-  def move(_, from), do: from 
+  def move("U", from, :rectangle) when from in ["4", "5", "6", "7", "8", "9"], do: do_move(from, &(&1 - 3))
+  def move("D", from, :rectangle) when from in ["1", "2", "3", "4", "5", "6"], do: do_move(from, &(&1 + 3))
+  def move("L", from, :rectangle) when from in ["2", "3", "5", "6", "8", "9"], do: do_move(from, &(&1 - 1))
+  def move("R", from, :rectangle) when from in ["1", "2", "4", "5", "7", "8"], do: do_move(from, &(&1 + 1))
+
+  def move("L", from, :diamond) when from in ["3", "4", "6", "7", "8", "9", "B", "C"], do:
+  do_move(from, &(&1 - 1))
+  def move("R", from, :diamond) when from in ["2", "3", "5", "6", "7", "8", "A", "B"], do:
+  do_move(from, &(&1 + 1))
+
+  def move("U", "3",  :diamond), do: "1"
+  def move("U", from, :diamond) when from in ["6", "7", "8"], do: do_move(from, &(&1 - 4))
+  def move("U", from, :diamond) when from in ["A", "B", "C"], do: do_move(from, &(&1 - 4 - 7))
+  def move("U", "D",  :diamond), do: "B"
+
+  def move("D", "1",  :diamond), do: "3"
+  def move("D", from, :diamond) when from in ["2", "3", "4"], do: do_move(from, &(&1 + 4))
+  def move("D", from, :diamond) when from in ["6", "7", "8"], do: do_move(from, &(&1 + 4 + 7))
+  def move("D", "B",  :diamond), do: "D"
+
+  def move(_move, from, _keypad_shape), do: from
+
+  defp do_move(from, func) do
+    from
+    |> to_charlist
+    |> hd
+    |> func.()
+    |> to_button
+  end
+
+  defp to_button(value), do: <<value>>
 end
--- day_02/tests.exs
+++ day_02/tests.exs
@@ -9,11 +9,11 @@ defmodule AcceptanceTest do
   end
 
   test "PART 1: for rectangle keypad password is 1985", %{instruction: instruction} do
-    assert instruction |> Keypad.to_code == "1985"
+    assert instruction |> Keypad.to_code(:rectangle) == "1985"
   end
 
   test "PART 2: for diamond keypad password is 5DB3", %{instruction: instruction} do
-    assert instruction |> Keypad.to_code == "5DB3"
+    assert instruction |> Keypad.to_code(:diamond) == "5DB3"
   end
 
 end
@@ -27,11 +27,11 @@ defmodule KeypadTest do
 
   describe "to_code" do
     test "instruction UUUU should return button 2" do
-      assert Keypad.to_code("UUUU") == "2"
+      assert Keypad.to_code("UUUU", :rectangle) == "2"
     end
 
     test "set up instruction should return 5287", %{instructions: instructions} do
-      assert instructions |> Keypad.to_code == "5287"
+      assert instructions |> Keypad.to_code(:rectangle) == "5287"
     end
   end
 end
@@ -44,7 +44,7 @@ defmodule InstructionTest do
 
   describe "to_sequence" do
     test "should convert string containing instructions to password sequence", %{instructions: instructions} do
-      assert instructions |> Instruction.to_sequence(5) == "5287"
+      assert instructions |> Instruction.to_sequence("5", :rectangle) == "5287"
     end
   end
 end
@@ -54,28 +54,67 @@ defmodule MoveTest do
 
   describe "move" do
     test "one button up from button 5 is button 2" do
-      assert Move.move("U", 5) == 2
+      assert Move.move("U", "5", :rectangle) == "2"
     end
 
     test "it is impossible to move one button up starting at any button in first row so should return same button" do
-      assert Move.move("U", 1) == 1
-      assert Move.move("U", 2) == 2
-      assert Move.move("U", 3) == 3
+      assert Move.move("U", "1", :rectangle) == "1"
+      assert Move.move("U", "2", :rectangle) == "2"
+      assert Move.move("U", "3", :rectangle) == "3"
     end
 
     test "all impossible moves should return same button" do
-      assert Move.move("U", 1) == 1
-      assert Move.move("U", 2) == 2
-      assert Move.move("U", 3) == 3
-      assert Move.move("R", 3) == 3
-      assert Move.move("R", 6) == 6
-      assert Move.move("R", 9) == 9
-      assert Move.move("D", 9) == 9
-      assert Move.move("D", 8) == 8
-      assert Move.move("D", 7) == 7
-      assert Move.move("L", 7) == 7
-      assert Move.move("L", 4) == 4
-      assert Move.move("L", 1) == 1
+      assert Move.move("U", "1", :rectangle) == "1"
+      assert Move.move("U", "2", :rectangle) == "2"
+      assert Move.move("U", "3", :rectangle) == "3"
+      assert Move.move("R", "3", :rectangle) == "3"
+      assert Move.move("R", "6", :rectangle) == "6"
+      assert Move.move("R", "9", :rectangle) == "9"
+      assert Move.move("D", "9", :rectangle) == "9"
+      assert Move.move("D", "8", :rectangle) == "8"
+      assert Move.move("D", "7", :rectangle) == "7"
+      assert Move.move("L", "7", :rectangle) == "7"
+      assert Move.move("L", "4", :rectangle) == "4"
+      assert Move.move("L", "1", :rectangle) == "1"
+    end
+  end
+
+  describe "diamond shape moves" do
+    test "every available move to left should return previous character" do
+      assert Move.move("L", "3", :diamond) == "2"
+      assert Move.move("L", "4", :diamond) == "3"
+      assert Move.move("L", "6", :diamond) == "5"
+      assert Move.move("L", "7", :diamond) == "6"
+      assert Move.move("L", "8", :diamond) == "7"
+      assert Move.move("L", "9", :diamond) == "8"
+      assert Move.move("L", "B", :diamond) == "A"
+      assert Move.move("L", "C", :diamond) == "B"
+    end
+    test "every available move to right should return next character" do
+      assert Move.move("R", "2", :diamond) == "3"
+      assert Move.move("R", "3", :diamond) == "4"
+      assert Move.move("R", "5", :diamond) == "6"
+      assert Move.move("R", "6", :diamond) == "7"
+      assert Move.move("R", "7", :diamond) == "8"
+      assert Move.move("R", "8", :diamond) == "9"
+      assert Move.move("R", "A", :diamond) == "B"
+      assert Move.move("R", "B", :diamond) == "C"
+    end
+    test "moving down from button 1 should return button 3" do
+      assert Move.move("D", "1", :diamond) == "3"
+    end
+    test "moving down from 2,3 or 4 should return 6,7 and 8 consequently" do
+      assert Move.move("D", "2", :diamond) == "6"
+      assert Move.move("D", "3", :diamond) == "7"
+      assert Move.move("D", "4", :diamond) == "8"
+    end
+    test "moving down from 6,7 or 8 should return A,B and C consequently" do
+      assert Move.move("D", "6", :diamond) == "A"
+      assert Move.move("D", "7", :diamond) == "B"
+      assert Move.move("D", "8", :diamond) == "C"
+    end
+    test "moving down from button B should return button D" do
+      assert Move.move("D", "B", :diamond) == "D"
     end
   end
 end
