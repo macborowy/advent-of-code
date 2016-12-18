b663f4a4399d0ec6c9fb30e5d13f42a542bd0215
Maciej Borowy <kontakt@mborowy.com>
2016-12-18 23:13:05 +0100

    refactor code

--- day_03/script.exs
+++ day_03/script.exs
@@ -1,28 +1,28 @@
 defmodule Script do
   def select_triangles(input) do
     input
-    |> String.split("\n")
-    |> Enum.map(&get_sides/1)
-    |> group_by_columns
-    |> Enum.filter(&is_triangle?/1)
+    |> parse
+    |> select
   end
 
-  defp get_sides(""), do: [0, 0, 0]
-  defp get_sides(str) do
-    Regex.run(~r/\s+(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})/, str)
-    |> tl
-    |> Enum.map(&String.to_integer/1)
-  end
-
-  defp group_by_columns(list) do
-    list
-    |> Enum.chunk(3)
-    |> Enum.map(&transpose/1)
+  defp parse(input) do
+    Regex.scan(~r/(\d+)/, input, capture: :first)
     |> List.flatten
+    |> Enum.map(&String.to_integer/1)
+    |> make_triangles
+  end
+
+  defp make_triangles(list) do
+    # based on https://github.com/jeg2/advent_of_code_2016 idea
+    Enum.concat([
+      list |> Enum.take_every(3),
+      list |> Enum.drop(1) |> Enum.take_every(3),
+      list |> Enum.drop(2) |> Enum.take_every(3)
+    ])
     |> Enum.chunk(3)
   end
 
-  defp transpose([[a, b, c], [d, e, f], [g, h, i]]), do: [[a, d, g], [b, e, h], [c, f, i]]
+  defp select(list), do: list |> Enum.filter(&is_triangle?/1)
 
   defp is_triangle?([a, b, c]) when a + b > c and a + c > b and b + c > a, do: true
   defp is_triangle?(_), do: false
