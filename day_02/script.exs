7d84f73cf355db988460d0595a591f91b779f128
Maciej Borowy <kontakt@mborowy.com>
2016-12-18 16:35:38 +0100

    prepared code for second part of exercise

--- day_02/script.exs
+++ day_02/script.exs
@@ -1,9 +1,15 @@
 defmodule Keypad do
   def to_code(instructions) do
     instructions
+    |> Instruction.to_sequence(5)
+  end
+end
+
+defmodule Instruction do
+  def to_sequence(instructions, from) do
+    instructions
     |> parse
-    |> find_sequence(5)
-    |> to_int
+    |> find_sequence(from)
   end
 
   defp parse(instructions) do
@@ -15,20 +21,21 @@ defmodule Keypad do
 
   defp find_sequence(instructions, from) do
     instructions
-    |> Enum.reduce([from], &make_sequence/2)
+    |> Enum.reduce([from], &reduce_to_sequence/2)
     |> Enum.reverse
     |> tl
+    |> Enum.join
   end
 
-  defp make_sequence(moves, [from | _] = acc), do: [find_button(moves, from) | acc]
+  defp reduce_to_sequence(moves, [from | _] = acc), do: [find_button(moves, from) | acc]
 
-  defp find_button(moves, from), do: moves |> Enum.reduce(from, &move/2)
+  defp find_button(moves, from), do: moves |> Enum.reduce(from, &Move.move/2)
+end
 
-  defp move("U", from) when from in [4, 5, 6, 7, 8, 9], do: from - 3
-  defp move("D", from) when from in [1, 2, 3, 4, 5, 6], do: from + 3
-  defp move("L", from) when from in [2, 3, 5, 6, 8, 9], do: from - 1
-  defp move("R", from) when from in [1, 2, 4, 5, 7, 8], do: from + 1
-  defp move(_, from), do: from
-
-  defp to_int(sequence), do: sequence |> Enum.join |> String.to_integer
+defmodule Move do
+  def move("U", from) when from in [4, 5, 6, 7, 8, 9], do: from - 3
+  def move("D", from) when from in [1, 2, 3, 4, 5, 6], do: from + 3
+  def move("L", from) when from in [2, 3, 5, 6, 8, 9], do: from - 1
+  def move("R", from) when from in [1, 2, 4, 5, 7, 8], do: from + 1
+  def move(_, from), do: from 
 end
--- day_02/tests.exs
+++ day_02/tests.exs
@@ -4,9 +4,18 @@ ExUnit.start
 defmodule AcceptanceTest do
   use ExUnit.Case
 
-  test "should convert given instructions to 1985" do
-    assert "ULL\nRRDDD\nLURDL\nUUUUD\n" |> Keypad.to_code == 1985
+  setup do
+    %{instruction: "ULL\nRRDDD\nLURDL\nUUUUD\n"}
   end
+
+  test "PART 1: for rectangle keypad password is 1985", %{instruction: instruction} do
+    assert instruction |> Keypad.to_code == "1985"
+  end
+
+  test "PART 2: for diamond keypad password is 5DB3", %{instruction: instruction} do
+    assert instruction |> Keypad.to_code == "5DB3"
+  end
+
 end
 
 defmodule KeypadTest do
@@ -18,54 +27,55 @@ defmodule KeypadTest do
 
   describe "to_code" do
     test "instruction UUUU should return button 2" do
-      assert Keypad.to_code("UUUU") == 2
+      assert Keypad.to_code("UUUU") == "2"
     end
 
     test "set up instruction should return 5287", %{instructions: instructions} do
-      assert instructions |> Keypad.to_code == 5287
+      assert instructions |> Keypad.to_code == "5287"
     end
   end
+end
+defmodule InstructionTest do
+  use ExUnit.Case
 
-  # describe "parse" do
-  #   test "should split lines by new line", %{instructions: instructions} do
-  #     assert instructions |> Keypad.parse |> length == 4
-  #   end
+  setup do
+    %{instructions: "URDL\nUUUU\nDDDD\nLLLL"}
+  end
 
-  #   test "each element should be list of moves to do", %{instructions: instructions} do
-  #     assert instructions |> Keypad.parse |> hd == ["U", "R", "D", "L"]
-  #   end
-  # end
+  describe "to_sequence" do
+    test "should convert string containing instructions to password sequence", %{instructions: instructions} do
+      assert instructions |> Instruction.to_sequence(5) == "5287"
+    end
+  end
+end
 
-  # describe "move" do
-  #   test "one button up from button 5 is button 2" do
-  #     assert Keypad.move("U", 5) == 2
-  #   end
+defmodule MoveTest do
+  use ExUnit.Case
 
-  #   test "it is impossible to move one button up starting at any button in first row so should return same button" do
-  #     assert Keypad.move("U", 1) == 1
-  #     assert Keypad.move("U", 2) == 2
-  #     assert Keypad.move("U", 3) == 3
-  #   end
+  describe "move" do
+    test "one button up from button 5 is button 2" do
+      assert Move.move("U", 5) == 2
+    end
 
-  #   test "all impossible moves should return same button" do
-  #     assert Keypad.move("U", 1) == 1
-  #     assert Keypad.move("U", 2) == 2
-  #     assert Keypad.move("U", 3) == 3
-  #     assert Keypad.move("R", 3) == 3
-  #     assert Keypad.move("R", 6) == 6
-  #     assert Keypad.move("R", 9) == 9
-  #     assert Keypad.move("D", 9) == 9
-  #     assert Keypad.move("D", 8) == 8
-  #     assert Keypad.move("D", 7) == 7
-  #     assert Keypad.move("L", 7) == 7
-  #     assert Keypad.move("L", 4) == 4
-  #     assert Keypad.move("L", 1) == 1
-  #   end
-  # end
+    test "it is impossible to move one button up starting at any button in first row so should return same button" do
+      assert Move.move("U", 1) == 1
+      assert Move.move("U", 2) == 2
+      assert Move.move("U", 3) == 3
+    end
 
-  # describe "find_button" do
-  #   test "starting at button 5 and having instruction UUUU should return button 2" do
-  #     assert Keypad.find_button(["U", "U", "U", "U"], 5) == 2
-  #   end
-  # end
+    test "all impossible moves should return same button" do
+      assert Move.move("U", 1) == 1
+      assert Move.move("U", 2) == 2
+      assert Move.move("U", 3) == 3
+      assert Move.move("R", 3) == 3
+      assert Move.move("R", 6) == 6
+      assert Move.move("R", 9) == 9
+      assert Move.move("D", 9) == 9
+      assert Move.move("D", 8) == 8
+      assert Move.move("D", 7) == 7
+      assert Move.move("L", 7) == 7
+      assert Move.move("L", 4) == 4
+      assert Move.move("L", 1) == 1
+    end
+  end
 end
