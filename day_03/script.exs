10830e1299ec58efe95776ae0153d2e25fab630b
Maciej Borowy <kontakt@mborowy.com>
2016-12-18 22:25:35 +0100

    part 2 of day 3 exercise

--- day_03/script.exs
+++ day_03/script.exs
@@ -3,6 +3,7 @@ defmodule Script do
     input
     |> String.split("\n")
     |> Enum.map(&get_sides/1)
+    |> group_by_columns
     |> Enum.filter(&is_triangle?/1)
   end
 
@@ -12,6 +13,16 @@ defmodule Script do
     |> tl
     |> Enum.map(&String.to_integer/1)
   end
+
+  defp group_by_columns(list) do
+    list
+    |> Enum.chunk(3)
+    |> Enum.map(&transpose/1)
+    |> List.flatten
+    |> Enum.chunk(3)
+  end
+
+  defp transpose([[a, b, c], [d, e, f], [g, h, i]]), do: [[a, d, g], [b, e, h], [c, f, i]]
 
   defp is_triangle?([a, b, c]) when a + b > c and a + c > b and b + c > a, do: true
   defp is_triangle?(_), do: false
