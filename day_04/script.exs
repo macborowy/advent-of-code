786fd55ab7178932d6077999c5f13bb317f5b474
Maciej Borowy <kontakt@mborowy.com>
2016-12-19 11:06:53 +0100

    first part of day 4 exercise

--- day_04/script.exs
+++ day_04/script.exs
@@ -1,5 +1,57 @@
+defmodule Script do
+  def get_sum_of_sectors(input) do
+    input
+    |> parse
+    |> Enum.filter(&Room.is_real?/1)
+    |> Enum.reduce(0, fn (encrypted_name, acc) -> encrypted_name |> Room.sector |> Kernel.+(acc) end)
+  end
+
+  defp parse(input), do: input |> String.trim |> String.split("\n")
+end
+
 defmodule Room do
   def is_real?(encrypted_name) do
-    
+    checksum = encrypted_name |> checksum
+    cipher = encrypted_name |> Cipher.create
+
+    cipher == checksum
+  end
+
+  def sector(input) do
+    Regex.run(~r/\-(\d+)\[/, input, capture: :all_but_first)
+    |> List.first
+    |> String.to_integer
+  end
+
+  defp checksum(input), do: Regex.run(~r/\[(\w+)\]/, input, capture: :all_but_first) |> List.first
+end
+
+defmodule Cipher do
+  def create(input) do
+    input
+    |> parse
+    |> get_most_common_letters
+    |> to_cipher
+  end
+
+  defp parse(input) do
+    input
+    |> String.replace(~r/[^a-z]/, "")
+    |> to_charlist
+    |> Enum.drop(-5)
+  end
+
+  defp get_most_common_letters(charlist) do
+    charlist
+    |> Enum.group_by(&(&1))
+    |> Enum.sort_by(fn {_, list} -> list |> length end, &>=/2)
+    |> Enum.take(5)
+  end
+
+  defp to_cipher(list) do
+    list
+    |> Enum.reduce([], fn ({letter, _}, acc) -> [letter | acc] end)
+    |> Enum.reverse
+    |> List.to_string
   end
 end
--- day_04/tests.exs
+++ day_04/tests.exs
@@ -4,10 +4,27 @@ ExUnit.start
 defmodule AcceptanceTest do
   use ExUnit.Case
 
+  test "should return sum of real room sectors" do
+    input = "aaaaa-bbb-z-y-x-123[abxyz]\na-b-c-d-e-f-g-h-987[abcde]\nnot-a-real-room-404[oarel]\ntotally-real-room-200[decoy]\n"
+
+    assert input |> Script.get_sum_of_sectors == 1514
+  end
+end
+
+defmodule RoomTest do
+  use ExUnit.Case
+
   test "check rooms" do
     assert Room.is_real?("aaaaa-bbb-z-y-x-123[abxyz]")
     assert Room.is_real?("a-b-c-d-e-f-g-h-987[abcde]")
-    refute Room.is_real?("not-a-real-room-404[oarel]")
+    assert Room.is_real?("not-a-real-room-404[oarel]")
     refute Room.is_real?("totally-real-room-200[decoy]")
+  end
+
+  test "get sectors" do
+    assert Room.sector("aaaaa-bbb-z-y-x-123[abxyz]") == 123
+    assert Room.sector("a-b-c-d-e-f-g-h-987[abcde]") == 987
+    assert Room.sector("not-a-real-room-404[oarel]") == 404
+    assert Room.sector("totally-real-room-200[decoy]") == 200
   end
 end
