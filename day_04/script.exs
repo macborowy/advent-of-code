eee1063e2636a1f18ea2b4633fa66210e47454d6
Maciej Borowy <kontakt@mborowy.com>
2016-12-19 11:55:27 +0100

    add part 2 of day 4 exercise

--- day_04/script.exs
+++ day_04/script.exs
@@ -2,14 +2,40 @@ defmodule Script do
   def get_sum_of_sectors(input) do
     input
     |> parse
-    |> Enum.filter(&Room.is_real?/1)
+    |> filter_real_rooms
     |> Enum.reduce(0, fn (encrypted_name, acc) -> encrypted_name |> Room.sector |> Kernel.+(acc) end)
   end
 
+  def get_real_room_names(input) do
+    input
+    |> parse
+    |> filter_real_rooms
+    |> Enum.map(&Room.encrypted_name/1)
+    |> Enum.map(&Room.real_name/1)
+  end
+
   defp parse(input), do: input |> String.trim |> String.split("\n")
+
+  defp filter_real_rooms(list), do: list |> Enum.filter(&Room.is_real?/1)
 end
 
 defmodule Room do
+  def real_name(encrypted_name) do
+    sector = encrypted_name |> sector
+
+    real_name =
+      encrypted_name
+      |> String.split_at(-3)
+      |> elem(0)
+      |> String.replace("-", " ")
+      |> String.trim
+      |> String.graphemes
+      |> Enum.map(fn letter -> Shift.forward(letter, sector) end)
+      |> List.to_string
+
+    {real_name, sector}
+  end
+
   def is_real?(encrypted_name) do
     checksum = encrypted_name |> checksum
     cipher = encrypted_name |> Cipher.create
@@ -17,11 +43,13 @@ defmodule Room do
     cipher == checksum
   end
 
-  def sector(input) do
-    Regex.run(~r/\-(\d+)\[/, input, capture: :all_but_first)
+  def sector(encrypted_name) do
+    Regex.run(~r/\-(\d+)/, encrypted_name, capture: :all_but_first)
     |> List.first
     |> String.to_integer
   end
+
+  def encrypted_name(name_with_checksum), do: name_with_checksum |> String.split_at(-7) |> elem(0)
 
   defp checksum(input), do: Regex.run(~r/\[(\w+)\]/, input, capture: :all_but_first) |> List.first
 end
@@ -55,3 +83,18 @@ defmodule Cipher do
     |> List.to_string
   end
 end
+
+
+defmodule Shift do
+  def forward(" ", _), do: " "
+  def forward(letter = <<_::8>>, times) do
+    value = letter |> to_charlist |> hd
+    shift = rem(times, 26)
+
+    cond do
+      value + shift > 122 -> <<value + shift - 26>>
+      true -> <<value + shift>>
+    end
+  end
+end
+
--- day_04/tests.exs
+++ day_04/tests.exs
@@ -1,6 +1,22 @@
 Code.load_file("script.exs")
 ExUnit.start
 
+defmodule RegresionTest do
+  use ExUnit.Case
+
+  test "part 1 answer" do
+    assert 278221 == "input.txt" |> File.read! |> Script.get_sum_of_sectors
+  end
+
+  test "part 2 answer" do
+    assert 267 == "input.txt"
+    |> File.read!
+    |> Script.get_real_room_names
+    |> Enum.find(fn {room, _} -> room == "northpole object storage" end)
+    |> elem(1)
+  end
+end
+
 defmodule AcceptanceTest do
   use ExUnit.Case
 
@@ -8,6 +24,13 @@ defmodule AcceptanceTest do
     input = "aaaaa-bbb-z-y-x-123[abxyz]\na-b-c-d-e-f-g-h-987[abcde]\nnot-a-real-room-404[oarel]\ntotally-real-room-200[decoy]\n"
 
     assert input |> Script.get_sum_of_sectors == 1514
+  end
+
+  test "should decipher room name" do
+    encrypted_name = "qzmt-zixmtkozy-ivhz-343"
+    expected ="very encrypted name"
+
+    assert {expected, 343} == encrypted_name |> Room.real_name
   end
 end
 
@@ -26,5 +49,27 @@ defmodule RoomTest do
     assert Room.sector("a-b-c-d-e-f-g-h-987[abcde]") == 987
     assert Room.sector("not-a-real-room-404[oarel]") == 404
     assert Room.sector("totally-real-room-200[decoy]") == 200
+  end
+end
+
+defmodule ShiftTest do
+  use ExUnit.Case
+
+  describe "shift" do
+    test "shifting a 27 letters forward returns b" do
+      assert Shift.forward("a", 27) == "b"
+    end
+
+    test "shifting z 1 letter forward returns a" do
+      assert Shift.forward("z", 1) == "a"
+    end
+
+    test "shifting a 343 letters forward returns f" do
+      assert Shift.forward("a", 343) == "f"
+    end
+
+    test "shifting space always return space" do
+      assert Shift.forward(" ", 1100) == " "
+    end
   end
 end
