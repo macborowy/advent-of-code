e72c5af7455055e4b4882aa8fbdb5e07e76b08e3
Maciej Borowy <kontakt@mborowy.com>
2016-12-20 18:53:42 +0100

    add just working solution to 2nd part

--- day_05/script.exs
+++ day_05/script.exs
@@ -15,3 +15,46 @@ defmodule Password do
     end
   end
 end
+
+defmodule AdvancedPassword do
+  def make(seed, tail \\ 0, password_length \\ 8) do
+    password_length
+    |> Keylock.new
+    |> do_make(seed, tail)
+    |> Tuple.to_list
+    |> Enum.join
+  end
+
+  defp do_make(keylock, seed, tail) do
+    case Keylock.is_valid?(keylock) do
+      false ->
+        {new_tail, pos, value} = find_value_somehow(seed, tail)
+        keylock
+        |> Keylock.update(pos, value)
+        |> IO.inspect
+        |> do_make(seed, new_tail)
+      _ ->
+        keylock
+    end
+  end
+
+  defp find_value_somehow(seed, tail) do
+    case MD5.hash(seed <> "#{tail}") do
+      "00000" <> <<pos::8>> <> <<val::8>> <> _rest when pos in 48..57 ->
+        pos = <<pos>> |> String.to_integer
+        {tail + 1, pos, <<val>>}
+      _ -> find_value_somehow(seed, tail + 1)
+    end
+  end
+end
+
+defmodule Keylock do
+  def new(spaces), do: Tuple.duplicate(nil, spaces)
+
+  def update(keylock, pos, val) when elem(keylock, pos) == nil do
+    put_elem(keylock, pos,val)
+  end
+  def update(keylock, _, _), do: keylock
+
+  def is_valid?(keylock), do: keylock |> Tuple.to_list |> Enum.all?(&(&1!=nil))
+end
--- day_05/tests.exs
+++ day_05/tests.exs
@@ -1,5 +1,6 @@
 Code.load_file("script.exs")
 ExUnit.start
+ExUnit.configure exclude: :long_running, trace: true
 
 defmodule PasswordTest do
   use ExUnit.Case
@@ -11,14 +12,17 @@ defmodule PasswordTest do
     }
   end
 
+  @tag :long_running
   test "every password should be 8 charaters long", %{seed: seed} do
     assert 8 == seed |> Password.make |> byte_size
   end
 
+  @tag :long_running
   test "for seed should return provided password", %{seed: seed, password: password} do
     assert password == seed |> Password.make
   end
 
+  @tag :long_running
   test "for given seed first character of password is 1", %{seed: seed} do
     assert "1" == seed |> Password.make(1)
   end
@@ -40,5 +44,75 @@ defmodule MD5Test do
 
   test "text should return provided hash", %{text: text, hash: hash} do
     assert hash == text |> MD5.hash
+  end
+end
+
+defmodule AdvancedPasswordTest do
+  use ExUnit.Case
+
+  setup do
+    %{
+      exercise_text: "abc",
+      hash_exercise_text: "0000000EA49FD3FC1B2F10E02D98EE96"
+    }
+  end
+
+  test "password has option to specify length", %{exercise_text: text} do
+    assert 1 == text |> AdvancedPassword.make(8605820, 1) |> String.length
+  end
+
+  test "password of length 1 should contain E on index 0", %{exercise_text: text} do
+    assert "0" == text |> AdvancedPassword.make(8605820, 1)
+  end
+
+  @tag :long_running
+  test "for excercise description seed should return valid password", %{exercise_text: text} do
+    assert "05ACE8E3" == "abc" |> AdvancedPassword.make
+  end
+end
+
+defmodule KeylockTest do
+  use ExUnit.Case
+
+  setup do
+    %{
+      tuple: {nil, nil, nil},
+      tuple_with_value: {nil, "1", nil}
+    }
+  end
+
+  describe "new" do
+    test "every keylock is a tuple" do
+      assert Keylock.new(8) |> is_tuple
+    end
+
+    test "keylock should be of requested length" do
+      assert 3 == Keylock.new(3) |> tuple_size
+    end
+
+    test "created keylock should contain only nils" do
+      assert {nil, nil, nil} == Keylock.new(3)
+    end
+  end
+
+  describe "update" do
+    test "should put value at position", %{tuple: tuple} do
+      assert {nil, "1", nil} == tuple |> Keylock.update(1, "1")
+    end
+
+    test "if position has value should not update", %{tuple_with_value: tuple_with_value} do
+      assert tuple_with_value == tuple_with_value |> Keylock.update(1, "100")
+    end
+
+    test "it is impossible to update not existing keylock position and should return same keylock tuple" do
+      assert {nil, nil} == {nil, nil} |> Keylock.update(3, 1)
+    end
+  end
+
+  describe "is_valid?" do
+    test "keylock is valid when every space has value other than nil", %{tuple_with_value: invalid_tuple} do
+      assert {"1", "2", "3"} |> Keylock.is_valid?
+      refute invalid_tuple |> Keylock.is_valid?
+    end
   end
 end
