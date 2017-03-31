e0973b41fac846f8a06ead18d7ba62f3b17fe459
Maciej Borowy <contact@macborowy.com>
2017-03-31 21:28:48 +0200

    add first part of exercise implementation

--- 2015/day_01/code.exs
+++ 2015/day_01/code.exs
@@ -1,3 +1,13 @@
 defmodule Exercise do
-  def solve(input), do: nil 
+  def solve(input) do
+    input
+    |> String.codepoints
+    |> Enum.reduce(0, &reducer/2)
+  end
+
+  def reducer("(", acc), do: acc + 1
+  def reducer(")", acc), do: acc - 1
+  def reducer(_, acc),   do: acc
 end
+
+"input.txt" |> File.read! |> Exercise.solve |> IO.puts 
