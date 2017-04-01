c9047d0556ebf19a64959b139689feb7a0a12aca
Maciej Borowy <contact@macborowy.com>
2017-04-01 14:10:51 +0200

    add delivery logic

--- 2015/day_03/code.exs
+++ 2015/day_03/code.exs
@@ -1,3 +1,25 @@
-defmodule Exercise do
-   
+defmodule Delivery do
+  @starting_location {0, 0}
+
+  def start(location \\ {0, 0}), do: [location]
+
+  def move(visited_locations = [{x, y} | _], "^"), do: [{x, y + 1} | visited_locations]
+  def move(visited_locations = [{x, y} | _], "<"), do: [{x - 1, y} | visited_locations]
+  def move(visited_locations = [{x, y} | _], "v"), do: [{x, y - 1} | visited_locations]
+  def move(visited_locations = [{x, y} | _], ">"), do: [{x + 1, y} | visited_locations]
+  def move(visited_locations, _), do: visited_locations
+
+  def visit_all(instructions) do
+    instructions
+    |> String.split("")
+    |> Enum.reduce([@starting_location], fn direction, acc -> move(acc, direction) end)
+  end
 end
+
+"input.txt"
+|> File.read!
+|> Delivery.visit_all
+|> Enum.group_by(&(&1))
+|> Map.keys
+|> Enum.count
+|> IO.puts
--- 2015/day_03/tests.exs
+++ 2015/day_03/tests.exs
@@ -3,5 +3,34 @@ ExUnit.start
 
 defmodule ModuleTest do
   use ExUnit.Case
+
+  test "Santa always visits house at starting location" do
+    assert [{0, 0}] == Delivery.start
+  end
+
+  describe "move tests" do
+    test "instruction ^ means visit house to north" do
+      assert Delivery.start |> Delivery.move("^") |> Enum.any?(&(&1 == {0, 1}))
+    end
+
+    test "instruction < means visit house to north" do
+      assert Delivery.start |> Delivery.move("<") |> Enum.any?(&(&1 == {-1, 0}))
+    end
+
+    test "instruction V means visit house to north" do
+      assert Delivery.start |> Delivery.move("V") |> Enum.any?(&(&1 == {0, -1}))
+    end
+
+    test "instruction > means visit house to east" do
+      assert Delivery.start |> Delivery.move(">") |> Enum.any?(&(&1 == {1, 0}))
+    end
+  end
+
+  test "when Santa has one move instruction he visits two houses" do
+    assert 2 == Delivery.start |> Delivery.move(">") |> Enum.count
+  end
+
+  test "for ^V^V^V^V^V instruction Santa visits only two houses" do
+    assert Delivery.visit_all("^V^V^V^V^V") |> Enum.all?(&(&1 == {0, 0} || &1 == {0, 1}))
   end
 end
