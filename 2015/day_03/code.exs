75568423efa09be40e9368667ba826b21a120ac7
Maciej Borowy <contact@macborowy.com>
2017-04-01 19:18:22 +0200

    refactored first part of the exercise

--- 2015/day_03/code.exs
+++ 2015/day_03/code.exs
@@ -1,19 +1,17 @@
 defmodule Delivery do
   @starting_location {0, 0}
 
-  def start(location \\ {0, 0}), do: [location]
-
-  def move(visited_locations = [{x, y} | _], "^"), do: [{x, y + 1} | visited_locations]
-  def move(visited_locations = [{x, y} | _], "<"), do: [{x - 1, y} | visited_locations]
-  def move(visited_locations = [{x, y} | _], "v"), do: [{x, y - 1} | visited_locations]
-  def move(visited_locations = [{x, y} | _], ">"), do: [{x + 1, y} | visited_locations]
-  def move(visited_locations, _), do: visited_locations
-
   def visit_all(instructions) do
     instructions
     |> String.split("")
     |> Enum.reduce([@starting_location], fn direction, acc -> move(acc, direction) end)
   end
+
+  defp move(visited = [{x, y} | _], "^"), do: [{x, y + 1} | visited]
+  defp move(visited = [{x, y} | _], "<"), do: [{x - 1, y} | visited]
+  defp move(visited = [{x, y} | _], "v"), do: [{x, y - 1} | visited]
+  defp move(visited = [{x, y} | _], ">"), do: [{x + 1, y} | visited]
+  defp move(visited, _), do: visited
 end
 
 "input.txt"
--- 2015/day_03/tests.exs
+++ 2015/day_03/tests.exs
@@ -4,33 +4,35 @@ ExUnit.start
 defmodule ModuleTest do
   use ExUnit.Case
 
-  test "Santa always visits house at starting location" do
-    assert [{0, 0}] == Delivery.start
+  test "Santa starting location is {0, 0}" do
+    houses_visited = Delivery.visit_all("")
+    assert 1 == houses_visited |> Enum.count
+    assert {0, 0} == houses_visited |> List.first
   end
 
   describe "move tests" do
     test "instruction ^ means visit house to north" do
-      assert Delivery.start |> Delivery.move("^") |> Enum.any?(&(&1 == {0, 1}))
+      assert Delivery.visit_all("^") |> Enum.any?(&(&1 == {0, 1}))
     end
 
     test "instruction < means visit house to north" do
-      assert Delivery.start |> Delivery.move("<") |> Enum.any?(&(&1 == {-1, 0}))
+      assert Delivery.visit_all("<") |> Enum.any?(&(&1 == {-1, 0}))
     end
 
     test "instruction V means visit house to north" do
-      assert Delivery.start |> Delivery.move("V") |> Enum.any?(&(&1 == {0, -1}))
+      assert Delivery.visit_all("v") |> Enum.any?(&(&1 == {0, -1}))
     end
 
     test "instruction > means visit house to east" do
-      assert Delivery.start |> Delivery.move(">") |> Enum.any?(&(&1 == {1, 0}))
+      assert Delivery.visit_all(">") |> Enum.any?(&(&1 == {1, 0}))
     end
   end
 
   test "when Santa has one move instruction he visits two houses" do
-    assert 2 == Delivery.start |> Delivery.move(">") |> Enum.count
+    assert 2 == Delivery.visit_all(">") |> Enum.count
   end
 
   test "for ^V^V^V^V^V instruction Santa visits only two houses" do
-    assert Delivery.visit_all("^V^V^V^V^V") |> Enum.all?(&(&1 == {0, 0} || &1 == {0, 1}))
+    assert Delivery.visit_all("^v^v^v^v^v") |> Enum.all?(&(&1 == {0, 0} || &1 == {0, 1}))
   end
 end
