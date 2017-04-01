e4cf6bc81f43c51359f6a2dd2a68b27946191372
Maciej Borowy <contact@macborowy.com>
2017-04-01 11:11:40 +0200

    add calculating required paper for wrapping present

--- 2015/day_02/code.exs
+++ 2015/day_02/code.exs
@@ -1,3 +1,26 @@
-defmodule Exercise do
-   
+defmodule Present do
+   def required_paper(dimensions) do
+     dimensions
+     |> parse
+     |> calculate
+   end
+
+   defp parse(dimensions) do
+     dimensions |> String.split("x") |> Enum.map(&String.to_integer/1)
+   end
+
+   defp calculate(dimensions = [a, b, c]) do
+      2*a*b + 2*a*c + 2*b*c + slack(dimensions)
+   end
+
+   defp slack(dimensions) do
+      [a, b, _] = dimensions |> Enum.sort(&(&1<&2))
+      a * b
+   end
 end
+
+"input.txt"
+|> File.read!
+|> String.split("\n", trim: true)
+|> Enum.reduce(0, fn (x, acc) -> acc + Present.required_paper(x) end)
+|> IO.puts
--- 2015/day_02/tests.exs
+++ 2015/day_02/tests.exs
@@ -4,7 +4,13 @@ ExUnit.start
 defmodule ModuleTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 == 1
+  describe "acceptance tests" do
+    test "present with dimensions 2x3x4 requires 58 square feet of wrapping paper" do
+      assert 58 == Present.required_paper("2x3x4")
+    end
+
+    test "present with dimensions 1x1x10 requires 43 square feet of wrapping paper" do
+      assert 43 == Present.required_paper("1x1x10") 
+    end
   end
 end
