ff18525cce6aad9408df2c78559b18e0b037f81c
Maciej Borowy <contact@macborowy.com>
2017-04-01 11:39:49 +0200

    add calculating required ribbon; remove slack/1

--- 2015/day_02/code.exs
+++ 2015/day_02/code.exs
@@ -1,26 +1,29 @@
 defmodule Present do
-   def required_paper(dimensions) do
-     dimensions
-     |> parse
-     |> calculate
-   end
+   def required_paper(dimensions), do:
+     dimensions |> parse |> calculate_required_paper
 
-   defp parse(dimensions) do
+   def required_ribbon(dimensions), do:
+     dimensions |> parse |> calculate_required_ribbon
+
+   defp parse(dimensions), do:
      dimensions |> String.split("x") |> Enum.map(&String.to_integer/1)
+
+   defp calculate_required_paper(dimensions = [a, b, c]) do
+     [x, y] = dimensions |> two_shortest_sides
+     2 * a * b + 2 * a * c + 2 * b * c + x * y
    end
 
-   defp calculate(dimensions = [a, b, c]) do
-      2*a*b + 2*a*c + 2*b*c + slack(dimensions)
+   defp calculate_required_ribbon(dimensions = [a, b, c]) do
+     [x, y] = dimensions |> two_shortest_sides
+     a * b * c + 2 * (x + y)
    end
 
-   defp slack(dimensions) do
-      [a, b, _] = dimensions |> Enum.sort(&(&1<&2))
-      a * b
-   end
+   defp two_shortest_sides(dimensions), do:
+     dimensions |> Enum.sort(&(&1<&2)) |> Enum.take(2)
 end
 
-"input.txt"
-|> File.read!
-|> String.split("\n", trim: true)
-|> Enum.reduce(0, fn (x, acc) -> acc + Present.required_paper(x) end)
-|> IO.puts
+input =  "input.txt" |> File.read!|> String.split("\n", trim: true)
+reduce = fn enum, func -> Enum.reduce(enum, 0, &(func.(&1) + &2)) end
+
+input |> reduce.(&Present.required_paper/1) |> IO.puts
+input |> reduce.(&Present.required_ribbon/1) |> IO.puts
--- 2015/day_02/tests.exs
+++ 2015/day_02/tests.exs
@@ -10,7 +10,15 @@ defmodule ModuleTest do
     end
 
     test "present with dimensions 1x1x10 requires 43 square feet of wrapping paper" do
-      assert 43 == Present.required_paper("1x1x10") 
+      assert 43 == Present.required_paper("1x1x10")
+    end
+
+    test "present with dimensions 2x3x4 requires 34 feets of ribbon" do
+      assert 34 == Present.required_ribbon("2x3x4")
+    end
+
+    test "present with dimensions 1x1x10 requires 14 feets of ribbon" do
+      assert 14 == Present.required_ribbon("1x1x10")
     end
   end
 end
