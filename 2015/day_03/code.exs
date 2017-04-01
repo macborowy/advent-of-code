3a26e7440108fd9d0a3e0908db1a33343d9c1032
Maciej Borowy <contact@macborowy.com>
2017-04-01 20:01:54 +0200

    refactor code

--- 2015/day_03/code.exs
+++ 2015/day_03/code.exs
@@ -2,21 +2,19 @@ defmodule Delivery do
   @starting_location {0, 0}
 
   def visit_all(instructions) when is_binary(instructions) do
-    instructions
-    |> String.split("")
-    |> Enum.reduce([@starting_location], fn direction, acc -> move(acc, direction) end)
+    instructions |> String.split("") |> Enum.reduce([@starting_location], &move/2)
   end
 
   def visit_all(instructions) when is_list(instructions) do
-    instructions |> Enum.reduce([@starting_location], fn direction, acc -> move(acc, direction) end)
+    instructions |> Enum.reduce([@starting_location], &move/2)
   end
 
   def visit_all(instructions, [help: :robosanta]) do
-    {santa, robosanta} = instructions |> String.split("") |> separate
-    visited_by_santa = santa |> visit_all
-    visited_by_robosanta = robosanta |> visit_all
-
-    Enum.concat(visited_by_santa, visited_by_robosanta)
+    instructions
+    |> String.split("")
+    |> separate
+    |> Enum.map(&visit_all/1)
+    |> List.flatten
   end
 
   def visit_all(_, keywords) do
@@ -24,23 +22,22 @@ defmodule Delivery do
      []
   end
 
-  # TODO: reverse parameters order and simplify Enum.reduce to ([], &move/2)
-  defp move(visited = [{x, y} | _], "^"), do: [{x, y + 1} | visited]
-  defp move(visited = [{x, y} | _], "<"), do: [{x - 1, y} | visited]
-  defp move(visited = [{x, y} | _], "v"), do: [{x, y - 1} | visited]
-  defp move(visited = [{x, y} | _], ">"), do: [{x + 1, y} | visited]
-  defp move(visited, _), do: visited
+  defp move("^", visited = [{x, y} | _]), do: [{x, y + 1} | visited]
+  defp move("<", visited = [{x, y} | _]), do: [{x - 1, y} | visited]
+  defp move("v", visited = [{x, y} | _]), do: [{x, y - 1} | visited]
+  defp move(">", visited = [{x, y} | _]), do: [{x + 1, y} | visited]
+  defp move(_,   visited), do: visited
 
   defp separate(instructions = [_ | rest]) do
     santa_path = instructions |> Enum.take_every(2)
     robosanta_path = rest |> Enum.take_every(2)
 
-    {santa_path, robosanta_path}
+    [santa_path, robosanta_path]
   end
 end
 
 input = "input.txt" |> File.read!
 parse_results = fn enum -> enum |> Enum.group_by(&(&1)) |> Map.keys |> Enum.count end
 
-input |> Delivery.visit_all |> parse_results.() |> IO.puts
+input |> Delivery.visit_all                   |> parse_results.() |> IO.puts
 input |> Delivery.visit_all(help: :robosanta) |> parse_results.() |> IO.puts
