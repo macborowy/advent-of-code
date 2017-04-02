25afa4fdaa68db34d7d8b1790715fbe89133e396
Maciej Borowy <contact@macborowy.com>
2017-04-02 13:49:02 +0200

    add finding AdventCoin answer value (both parts of the exercise)

--- 2015/day_04/code.exs
+++ 2015/day_04/code.exs
@@ -1,2 +1,22 @@
-defmodule Exercise do
+defmodule Miner do
+  def find_answer(secret, prefix \\ "00000", acc \\ 0) do
+    hash = hash(secret <> to_string(acc))
+    cond do
+      String.starts_with?(hash, prefix) -> acc
+      true                              -> find_answer(secret, prefix, acc + 1)
+    end
+  end
+
+  def hash(msg) do
+    :erlang.md5(msg)
+    |> :erlang.bitstring_to_list
+    |> Enum.map(&(:io_lib.format("~2.16.0b", [&1])))
+    |> List.flatten
+    |> to_string
+  end
+end
+
+if System.argv == ["--run"] do
+  "bgvyzdsv" |> Miner.find_answer |> IO.puts
+  "bgvyzdsv" |> Miner.find_answer("000000") |> IO.puts
 end
--- 2015/day_04/tests.exs
+++ 2015/day_04/tests.exs
@@ -1,5 +1,6 @@
 Code.load_file("code.exs")
 ExUnit.start
+ExUnit.configure trace: true
 
 defmodule ModuleTest do
   use ExUnit.Case
@@ -13,14 +14,14 @@ defmodule ModuleTest do
       secret = "abcdef"
       answer = 609043
 
-      assert answer == Miner.find_answer(secret)
+      assert answer == Miner.find_answer(secret, "00000", answer - 10)
     end
 
     test "for secret pqrstuv the answer is 1048970" do
       secret = "pqrstuv"
       answer = 1048970
 
-      assert answer == Miner.find_answer(secret)
+      assert answer == Miner.find_answer(secret, "00000", answer - 10)
     end
   end
 end
