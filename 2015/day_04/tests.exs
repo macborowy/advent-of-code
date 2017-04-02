1067f28b27ba70104625bce21fc9595530a0b34a
Maciej Borowy <contact@macborowy.com>
2017-04-02 12:15:23 +0200

    add acceptance tests

--- 2015/day_04/tests.exs
+++ 2015/day_04/tests.exs
@@ -7,4 +7,20 @@ defmodule ModuleTest do
   test "the truth" do
     assert 1 == 1
   end
+
+  describe "acceptance tests" do
+    test "for secret abcdef the answer is 609043" do
+      secret = "abcdef"
+      answer = 609043
+
+      assert answer == Miner.find_answer(secret)
+    end
+
+    test "for secret pqrstuv the answer is 1048970" do
+      secret = "pqrstuv"
+      answer = 1048970
+
+      assert answer == Miner.find_answer(secret)
+    end
+  end
 end
