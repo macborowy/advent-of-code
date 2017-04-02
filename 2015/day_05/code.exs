78597a3e9c114c49f94cc665698499a82179f26b
Maciej Borowy <contact@macborowy.com>
2017-04-02 16:22:17 +0200

    add test for vowels

--- 2015/day_05/code.exs
+++ 2015/day_05/code.exs
@@ -1,5 +1,30 @@
 defmodule Word do
-  
+  @vowels_reason "hasn't enough vowels (minimum 3 different)"
+
+  def nice?(word) when is_binary(word) do
+    nice? =
+      word
+      |> has_enough_vowels?
+
+    case nice? do
+      {:ok, _} -> true
+      error -> error
+    end
+  end
+  def nice?(_), do: {:err, "Parameter is not binary."}
+
+  defp has_enough_vowels?(word) do
+    nice? =
+      word
+      |> String.replace(~r/[^aeiou]/, "")
+      |> to_charlist
+      |> Enum.group_by(&(&1))
+      |> Map.keys
+      |> length
+      |> Kernel.>=(3)
+
+    if nice?, do: {:ok, word}, else: {:err, @vowels_reason}
+  end
 end
 
 if System.argv == ["--run"] do
--- 2015/day_05/tests.exs
+++ 2015/day_05/tests.exs
@@ -5,7 +5,24 @@ ExUnit.configure trace: true
 defmodule ModuleTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 == 1
+  test "when word is not binary, it is not nice" do
+    assert {:err, "Parameter is not binary."} == 1 |> Word.nice?
+  end
+
+  describe "vowels" do
+    test "word aei is nice because it has at least 3 vowels" do
+      assert "aei" |> Word.nice?
+    end
+
+    test "word with less than three vowels is not nice" do
+      assert :err == "aef" |> Word.nice? |> elem(0)
+      assert :err == "abc" |> Word.nice? |> elem(0)
+      assert :err == "bcd" |> Word.nice? |> elem(0)
+    end
+
+    test "when word hasn't enough vowels should return appropriate message" do
+      {:err, reason} = "consonants" |> Word.nice?
+      assert reason == "hasn't enough vowels (minimum 3 different)"
+    end
   end
 end
