5354ceb7c9cc4cc0ac07e20785440cd157dd13e3
Maciej Borowy <contact@macborowy.com>
2017-04-02 14:15:31 +0200

    add find_answer/3 doc and TODO

--- 2015/day_04/code.exs
+++ 2015/day_04/code.exs
@@ -1,4 +1,12 @@
 defmodule Miner do
+  @doc """
+  Searches for the answer to succesfully mine Santa's AdventCoin.
+
+  `secret` is a secret phrase you are looking for an answer, `prefix` is a prefix you know the right answer starts with, and `acc` is the value you are testing if it produces hash with provided prefix. If the hash test pass, value of the `acc` is the answer to mine a coin.
+
+  You can provide custom `prefix`, if test rules change, and custom `acc` (greater than 0), if you know that answer is greater or equals to this number.
+  """
+  # TODO: add concurrent hash testing support
   def find_answer(secret, prefix \\ "00000", acc \\ 0) do
     hash = hash(secret <> to_string(acc))
     cond do
