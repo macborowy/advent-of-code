c3f49f4d03bbeac600e5d3449a701dd9a5d597bf
Maciej Borowy <contact@macborowy.com>
2017-04-04 22:38:39 +0200

    add decoration initializer; add decoration command parsing

--- 2015/day_06/code.exs
+++ 2015/day_06/code.exs
@@ -1,3 +1,28 @@
-defmodule Exercise do
-   
+defmodule Command do
+  @command_regex ~r/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/
+  @atoms [:turn_on, :turn_off, :toggle]
+
+  defstruct [command: :turn_on, start_at: {0, 0}, finish_at: {0, 0}]
+
+  def new, do: %Command{}
+  def new([command, x1, y1, x2, y2]) do
+    %Command{
+      command:   command |> String.replace(" ", "_") |> String.to_existing_atom,
+      start_at:  {to_int(x1), to_int(y1)},
+      finish_at: {to_int(x2), to_int(y2)}
+    }
+  end
+
+  def parse(command) do
+    case Regex.scan(@command_regex, command) do
+      [[_ | results]] -> Command.new(results)
+      _               -> {:error, "invalid command: #{command}"}
+    end
+  end
+
+  defp to_int(string), do: String.to_integer(string)
+end
+
+defmodule Decoration do
+  def new, do: for x <- 0..999, y <- 0..999, do: {x, y}
 end
--- 2015/day_06/tests.exs
+++ 2015/day_06/tests.exs
@@ -2,6 +2,39 @@ Code.load_file("code.exs")
 ExUnit.start
 ExUnit.configure trace: true
 
-defmodule ExerciseTest do
+defmodule DecorationTest do
+  use ExUnit.Case
 
+  describe "new/0" do
+    test "should return milion coordinates" do
+      assert 1_000_000 == Decoration.new |> Enum.count
+    end
+  end
+end
+
+defmodule CommandTest do
+  use ExUnit.Case
+
+  describe "new" do
+    test "should return Command struct with default values" do
+      assert %Command{} == Command.new
+      assert %Command{command: :"turn_on", start_at: {0, 0}, finish_at: {0, 0}} == Command.new
+    end
+
+    test "should return Command struct with correct values" do
+      command_parameters = ["turn off", "1", "10", "100", "1000"]
+      assert %Command{command: :turn_off, start_at: {1, 10}, finish_at: {100, 1000}} == command_parameters |> Command.new
+    end
+  end
+
+  describe "parse" do
+    test "command turn on 0,0 through 999,999 should return turn on command and {0,0} and {999,999}" do
+      assert %Command{command: :turn_on, start_at: {0, 0}, finish_at: {999, 999}} == "turn on 0,0 through 999,999" |> Command.parse
+    end
+
+    test "should catch invalid command and return {:error, reason}" do
+      assert {:error, "invalid command: tarn on 0,0 through 999,999"} == "tarn on 0,0 through 999,999" |> Command.parse
+      assert {:error, "invalid command: turn on 0, through 999,999"} == "turn on 0, through 999,999" |> Command.parse
+    end
+  end
 end
