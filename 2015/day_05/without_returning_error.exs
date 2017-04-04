f77dd07533afe6244938bfbd2076df91d678cfcc
Maciej Borowy <contact@macborowy.com>
2017-04-04 20:46:06 +0200

    change with to use pipes; change method names

--- 2015/day_05/without_returning_error.exs
+++ 2015/day_05/without_returning_error.exs
@@ -1,8 +1,8 @@
 defmodule Word do
   def nice?(word) when is_binary(word) do
-    with true <- has_enough_vowels?(word),
-         true <- has_double_letters?(word),
-         true <- not_contain_disallowed_string(word),
+    with true <- word |> has_enough_vowels?,
+         true <- word |> has_double_letters?,
+         true <- word |> has_no_disallowed_string?,
          do: true
   end
 
@@ -15,13 +15,13 @@ defmodule Word do
 
   defp has_double_letters?(word), do: String.match?(word, ~r/(\p{L})\1/)
 
-  defp not_contain_disallowed_string(word), do: not String.match?(word, ~r/(ab|cd|pq|xy)/)
+  defp has_no_disallowed_string?(word), do: not String.match?(word, ~r/(ab|cd|pq|xy)/)
 end
 
 defmodule BetterWord do
   def nice?(word) when is_binary(word) do
-    with true <- has_at_least_two_pairs_of_letters?(word),
-         true <- has_repeat_with_single_letter_between?(word),
+    with true <- word |> has_at_least_two_pairs_of_letters?,
+         true <- word |> has_repeat_with_single_letter_between?,
     do: true
   end
 
