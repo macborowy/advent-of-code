93b404b7c9f74071b31eb9b5e8c236fd35d263f1
Maciej Borowy <contact@macborowy.com>
2017-04-05 22:43:18 +0200

    setting up decoration using brute force method

--- 2015/day_06/code.exs
+++ 2015/day_06/code.exs
@@ -2,12 +2,12 @@ defmodule Command do
   @command_regex ~r/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/
   @atoms [:turn_on, :turn_off, :toggle]
 
-  defstruct [command: :turn_on, start_at: {0, 0}, finish_at: {0, 0}]
+  defstruct [action: :turn_on, start_at: {0, 0}, finish_at: {0, 0}]
 
   def new, do: %Command{}
-  def new([command, x1, y1, x2, y2]) do
+  def new([action, x1, y1, x2, y2]) do
     %Command{
-      command:   command |> String.replace(" ", "_") |> String.to_existing_atom,
+      action:   action |> String.replace(" ", "_") |> String.to_existing_atom,
       start_at:  {to_int(x1), to_int(y1)},
       finish_at: {to_int(x2), to_int(y2)}
     }
@@ -24,5 +24,30 @@ defmodule Command do
 end
 
 defmodule Decoration do
-  def new, do: for x <- 0..999, y <- 0..999, do: {x, y}
+  def new, do: for x <- 0..999, y <- 0..999, do: {:turn_off, x, y}
+
+  def change(decoration_grid, command) do
+    %Command{action: action, start_at: {start_x, start_y}, finish_at: {finish_x, finish_y}} = Command.parse(command)
+
+    for light = {_, x, y} <- decoration_grid,
+        do: if x in start_x..finish_x && y in start_y..finish_y,
+            do: do_change(light, action),
+            else: light
+  end
+
+  defp do_change({_,         x, y}, :turn_on),  do: {:turn_on,  x, y}
+  defp do_change({_,         x, y}, :turn_off), do: {:turn_off, x, y}
+  defp do_change({:turn_on,  x, y}, :toggle),   do: {:turn_off, x, y}
+  defp do_change({:turn_off, x, y}, :toggle),   do: {:turn_on,  x, y}
+end
+
+if System.argv == ["--run"] do
+  "input.txt"
+  |> File.read!
+  |> String.trim
+  |> String.split("\n", trim: true)
+  # |> Enum.take(5)
+  |> Enum.reduce(Decoration.new, fn command, decoration -> Decoration.change(decoration, command) end)
+  |> Enum.count(&(:turn_on == &1 |> elem(0)))
+  |> IO.puts
 end
--- 2015/day_06/tests.exs
+++ 2015/day_06/tests.exs
@@ -5,9 +5,50 @@ ExUnit.configure trace: true
 defmodule DecorationTest do
   use ExUnit.Case
 
+  setup do
+    %{decoration_grid: Decoration.new}
+  end
+
   describe "new/0" do
     test "should return milion coordinates" do
       assert 1_000_000 == Decoration.new |> Enum.count
+    end
+
+    test "every light should be turned off" do
+      assert Decoration.new |> Enum.all?(fn x -> :turn_off == x |> elem(0) end)
+    end
+  end
+
+  describe "change" do
+    test "turn on 0,0 through 999,999 should turn on every light", %{decoration_grid: decoration_gird} do
+      command = "turn on 0,0 through 999,999"
+      assert 1_000_000 == decoration_gird
+                          |> Decoration.change(command)
+                          |> Enum.count(fn x -> :turn_on == x |> elem(0) end)
+    end
+
+    test "toggle 0,0 through 999,0 should toggle first column of lights", %{decoration_grid: decoration_gird} do
+      command = "toggle 0,0 through 999,0"
+      assert 1_000 == decoration_gird
+                      |> Decoration.change(command)
+                      |> Enum.count(&(:turn_on == &1 |> elem(0)))
+    end
+
+    test "should be possible to chain commands" do
+      first_command = "turn on 0,0 through 99,99"
+      second_command = "turn off 0,0 through 9,9"
+      third_command = "toggle 0,0 through 999,999"
+
+      final_decoration = Decoration.new
+                         |> Decoration.change(first_command)
+                         |> IO.inspect
+                         |> Decoration.change(second_command)
+                         |> IO.inspect
+                         |> Decoration.change(third_command)
+                         |> IO.inspect
+
+      assert 1_000_000 - (10_000 - 100) == final_decoration
+                                           |> Enum.count(&(:turn_on == &1 |> elem(0)))
     end
   end
 end
@@ -18,23 +59,27 @@ defmodule CommandTest do
   describe "new" do
     test "should return Command struct with default values" do
       assert %Command{} == Command.new
-      assert %Command{command: :"turn_on", start_at: {0, 0}, finish_at: {0, 0}} == Command.new
+      assert %Command{action: :turn_on, start_at: {0, 0}, finish_at: {0, 0}} == Command.new
     end
 
     test "should return Command struct with correct values" do
       command_parameters = ["turn off", "1", "10", "100", "1000"]
-      assert %Command{command: :turn_off, start_at: {1, 10}, finish_at: {100, 1000}} == command_parameters |> Command.new
+      assert %Command{action: :turn_off, start_at: {1, 10}, finish_at: {100, 1000}} == command_parameters |> Command.new
     end
   end
 
   describe "parse" do
     test "command turn on 0,0 through 999,999 should return turn on command and {0,0} and {999,999}" do
-      assert %Command{command: :turn_on, start_at: {0, 0}, finish_at: {999, 999}} == "turn on 0,0 through 999,999" |> Command.parse
+      assert %Command{action: :turn_on, start_at: {0, 0}, finish_at: {999, 999}} == "turn on 0,0 through 999,999" |> Command.parse
     end
 
     test "should catch invalid command and return {:error, reason}" do
       assert {:error, "invalid command: tarn on 0,0 through 999,999"} == "tarn on 0,0 through 999,999" |> Command.parse
       assert {:error, "invalid command: turn on 0, through 999,999"} == "turn on 0, through 999,999" |> Command.parse
+    end
+
+    test "should parse toogle command" do
+      assert %Command{action: :toggle, start_at: {0, 0}, finish_at: {999, 0}} == "toggle 0,0 through 999,0" |> Command.parse
     end
   end
 end
