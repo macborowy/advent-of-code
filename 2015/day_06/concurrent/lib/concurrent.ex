477d430cae6e9caf2d362eb3ade9e79126fc363a
Maciej Borowy <contact@macborowy.com>
2017-04-09 20:11:40 +0200

    add basic processing server; add test function in Concurrent module; add input.txt to /misc

--- 2015/day_06/concurrent/lib/concurrent.ex
+++ 2015/day_06/concurrent/lib/concurrent.ex
@@ -1,5 +1,10 @@
 defmodule Concurrent do
   @moduledoc false
 
-  
+  alias Server
+
+  def run do
+    {:ok, pid} = Server.start_link
+    result = Server.process(pid, "./misc/input.txt") |> IO.inspect
+  end
 end
---
+++ 2015/day_06/concurrent/test/server_test/server_test.exs
@@ -0,0 +1,30 @@
+defmodule ServerTest do
+  use ExUnit.Case, async: false
+
+
+  alias Server
+  alias Grid
+
+  setup do
+    {:ok, pid} = Server.start_link
+    %{pid: pid, path_to_file: "./misc/input.txt"}
+  end
+
+  describe "start_link" do
+    test "create server with default grid", %{pid: pid} do
+      %{grid: grid} = :sys.get_state(pid)
+      assert grid == Grid.new
+    end
+  end
+
+  describe "process" do
+    test "return grid after applying all commands", %{pid: pid, path_to_file: path} do
+      result = Server.process(pid, path)
+
+      assert 1000 == result |> length
+      assert 1000 == result |> Enum.at(0) |> length
+      assert result |> Enum.all?(&(&1 |> is_list))
+      assert result |> Enum.at(0) |> Enum.all?(&(&1 |> is_atom))
+    end
+  end
+end
