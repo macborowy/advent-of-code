8eee3144da20390d44c8cfa96c00e435719e8898
Maciej Borowy <contact@macborowy.com>
2017-05-02 17:36:23 +0200

    initial commit

---
+++ 2015/day_06/concurrent/lib/concurrent.ex
@@ -0,0 +1,18 @@
+defmodule Concurrent do
+  @moduledoc """
+  Documentation for Concurrent.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Concurrent.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
