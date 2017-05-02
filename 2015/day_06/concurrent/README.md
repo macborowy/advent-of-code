0914e78ae56dec569a641b895f1f39276e50b3b0
Maciej Borowy <contact@macborowy.com>
2017-05-02 17:34:56 +0200

    remove concurrent app - starting again

--- 2015/day_06/concurrent/README.md
+++
@@ -1,3 +0,0 @@
-# Concurrent
-
-**TODO: Add description**
--- 2015/day_06/concurrent/test/concurrent_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ConcurrentTest do
-  use ExUnit.Case
-  doctest Concurrent
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
