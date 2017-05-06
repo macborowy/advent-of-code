781a86e37ab1052cbe37d8dd949232cdaa1eed21
Maciej Borowy <contact@macborowy.com>
2017-05-06 14:50:16 +0200

    initial commit

---
+++ 2015/day_07/gates/lib/gates.ex
@@ -0,0 +1,18 @@
+defmodule Gates do
+  @moduledoc """
+  Documentation for Gates.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gates.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2015/day_07/gates/test/gates_test.exs
@@ -0,0 +1,8 @@
+defmodule GatesTest do
+  use ExUnit.Case
+  doctest Gates
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
