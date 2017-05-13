66e5e04be9acede5e9a6dc064ee854f6a77f72df
Maciej Borowy <contact@macborowy.com>
2017-05-13 20:03:54 +0200

    add implementation of Finder module

--- 2015/day_07/gates/lib/gates/finder.ex
+++ 2015/day_07/gates/lib/gates/finder.ex
@@ -1,13 +1,34 @@
 defmodule Gates.Finder do
-  def find([], _), do: :no_instructions
-  def find(instructions, wire) do
-    instructions
-    |> Enum.filter(fn %{input: input} ->
-      case input do
-        ^wire -> true
-        {^wire, shift} when is_integer(shift) -> true
-        _ -> false
-      end
-    end)
+
+  ####################
+  # Public Functions #
+  ####################
+
+  def find(instructions, :signals),
+    do: instructions |> Enum.filter(&is_signal?/1)
+
+  def find([], _, _), do: :no_instructions
+
+  def find(instructions, wire, known_wires) do
+    known_wires = known_wires |> Enum.map(fn {wire, _} -> wire end)
+    instructions |> Enum.filter(&single_node_and_pairs(&1, wire, known_wires))
   end
+
+  #####################
+  # Private Functions #
+  #####################
+
+  defp is_signal?(instruction) do
+    case instruction do
+      %{input: {signal, nil}} when is_integer(signal) -> true
+      %{input: {signal_1, signal_2}} when is_integer(signal_1) and is_integer(signal_2) -> true
+      _ -> false
+    end
+  end
+
+  defp single_node_and_pairs(%{input: {wire, other_wire}}, wire, known_wires), do:
+    other_wire == nil || is_integer(other_wire) || other_wire in known_wires
+  defp single_node_and_pairs(%{input: {other_wire, wire}}, wire, known_wires), do:
+    single_node_and_pairs(%{input: {wire, other_wire}}, wire, known_wires)
+  defp single_node_and_pairs(_, _, _), do: false
 end
--- 2015/day_07/gates/test/gates_test/finder_test.exs
+++ 2015/day_07/gates/test/gates_test/finder_test.exs
@@ -2,17 +2,24 @@ defmodule Gates.FinderTest do
   use ExUnit.Case
 
   alias Gates.Finder, as: Finder
+  alias Gates.Instruction, as: Instruction
 
   setup do
     input = """
     123 -> x
     456 -> y
-    x AND y -> d
-    y OR x -> e
+    x -> z
+    NOT 2 -> x
+    NOT x -> h
+    2 LSHIFT 3 -> d
     x LSHIFT 2 -> f
     y RSHIFT 2 -> g
-    NOT x -> h
-    NOT y -> i
+    2 AND y -> v
+    2 AND 3 -> t
+    x AND y -> d
+    q AND y -> b
+    2 AND x -> y
+    y OR x -> e
     """
 
     instructions = input |> String.split("\n", trim: true) |> Enum.map(&Gates.Instruction.new/1)
@@ -20,25 +27,56 @@ defmodule Gates.FinderTest do
     %{instructions: instructions}
   end
 
+
   test "when instructions list is empty should return :no_instructions" do
-    assert :no_instructions == [] |> Finder.find("a")
+    assert :no_instructions == Finder.find([], "a", [])
   end
 
   test "when there is no matching instruction return empty list", %{instructions: instructions} do
-    assert [] == instructions |> Finder.find("a")
+    assert [] == Finder.find(instructions, "a", [])
   end
 
-  test "when there is matching instruction should return all matches", %{instructions: instructions} do
-    result = instructions |> Finder.find("y")
+  test "when function first argument is :signals should return all instruction that are signal passing or signal mutation", %{instructions: instructions} do
+    expected = [
+      %Instruction{gate: nil,     input: {123, nil}, output: "x"},
+      %Instruction{gate: nil,     input: {456, nil}, output: "y"},
+      %Instruction{gate: :not,    input: {2, nil},   output: "x"},
+      %Instruction{gate: :lshift, input: {2, 3},     output: "d"},
+      %Instruction{gate: :and,    input: {2, 3},     output: "t"}
+    ]
 
-    assert 2 == result |> length
-    assert result |> Enum.all?(fn %{input: input} -> input == {"y", 2} or input == "y" end)
+    assert expected == Finder.find(instructions, :signals)
   end
 
-  test "for wires pair should return all matches, but not reversed", %{instructions: instructions} do
-    result = instructions |> Finder.find({"x", "y"})
+  test "when known_wires is empty should return all possible to solve instructions without depending on already known wires", %{instructions: instructions} do
+    expected = [
+      %Instruction{gate: nil,     input: {"x", nil}, output: "z"},
+      %Instruction{gate: :not,    input: {"x", nil}, output: "h"},
+      %Instruction{gate: :lshift, input: {"x", 2},   output: "f"},
+      %Instruction{gate: :and,    input: {2, "x"},   output: "y"}
+    ]
 
-    assert 1 == result |> length
-    assert result |> Enum.any?(fn %{input: input} -> input == {"x", "y"} end)
+    assert expected == Finder.find(instructions, "x", [])
+  end
+
+  test "when known_wires has wires that are not pairing with searching wire should return all instructions that aren't depend on other wire", %{instructions: instructions} do
+    known_wires = [{"a", 3}, {"b", 1}]
+    expected = [
+      %Instruction{gate: :rshift, input: {"y", 2},   output: "g"},
+      %Instruction{gate: :and,    input: {2, "y"},   output: "v"},
+    ]
+
+    assert expected == Finder.find(instructions, "y", known_wires)
+  end
+
+  test "when known_wires has wires that are pair with searching wire should return all instructions", %{instructions: instructions} do
+    known_wires = [{"q", 2}]
+    expected = [
+      %Instruction{gate: :rshift, input: {"y", 2},   output: "g"},
+      %Instruction{gate: :and,    input: {2, "y"},   output: "v"},
+      %Instruction{gate: :and,    input: {"q", "y"}, output: "b"}
+    ]
+
+    assert expected == Finder.find(instructions, "y", known_wires)
   end
 end
