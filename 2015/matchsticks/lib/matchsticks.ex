2515368fdc54422e8ade5e68e68693c04cddc23b
Maciej Borowy <contact@macborowy.com>
2017-06-11 18:59:26 +0200

    solution to first part

--- 2015/matchsticks/lib/matchsticks.ex
+++ 2015/matchsticks/lib/matchsticks.ex
@@ -1,18 +1,18 @@
 defmodule Matchsticks do
-  @moduledoc """
-  Documentation for Matchsticks.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Matchsticks.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def count_for(input) do
+    input
+    |> File.read!
+    |> String.split("\n")
+    |> Enum.reduce({0, 0}, &count/2)
   end
+
+  def count(input, result \\ {0, 0})
+  def count(<<>>, result), do: result
+  def count(<<"\"", rest::binary>>, {code, char}), do: count(rest, {code + 1, char})
+  def count(<<"\\\\", rest::binary>>, {code, char}), do: count(rest, {code + 2, char + 1})
+  def count(<<"\\\"", rest::binary>>, {code, char}), do: count(rest, {code + 2, char + 1})
+  def count(<<"\\x", hex::16, rest::binary>>, {code, char}), do: count(rest, {code + 4, char + 1})
+  def count(<<c::8, rest::binary>>, {code, char}), do: count(rest, {code + 1, char + 1})
 end
--- 2015/matchsticks/test/matchsticks_test.exs
+++ 2015/matchsticks/test/matchsticks_test.exs
@@ -2,7 +2,44 @@ defmodule MatchsticksTest do
   use ExUnit.Case
   doctest Matchsticks
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    strings = "input.txt" |> File.read! |> String.split("\n")
+    %{strings: strings, first: strings |> Enum.at(0)}
+  end
+
+  describe "acceptance tests" do
+    @tag :acceptance
+    test "two double quotes is two characters of code, but the string has no meaningful character" do
+      assert {2, 0} == Matchsticks.count("\"\"")
+    end
+
+    @tag :acceptance
+    test "\"abc\" is 5 characters of code, but 3 meaningful characters in the string data" do
+      assert {5, 3} == Matchsticks.count("\"abc\"")
+    end
+
+    @tag :acceptance
+    test "\"aaa\"aaa\" is 10 characters of code, but the string has 7 meaningful characters" do
+      assert {10, 7} == Matchsticks.count("\"aaa\\\"aaa\"")
+    end
+
+    @tag :acceptance
+    test "\"\x27\" is 6 characters of code, but has only one meaningful character" do
+      assert {6, 1} == Matchsticks.count("\"\\x27\"")
+    end
+  end
+
+  describe "tests with real input" do
+    test "for the first input string return correct values", %{first: first} do
+      assert {16, 7} == Matchsticks.count(first)
+    end
+  end
+
+  describe "test/1" do
+    test "every input has at least two character of code for two double quotes" do
+      assert {2, 0} == Matchsticks.count("\"\"")
+      assert {3, 1} == Matchsticks.count("\"1\"")
+      assert {2, 0} == Matchsticks.count(<<"\"\"">>)
+    end
   end
 end
