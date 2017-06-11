a0a86fbf2fe015b5a62edbb1c28b8fbb63b76111
Maciej Borowy <contact@macborowy.com>
2017-06-11 12:43:16 +0200

    refactor Finder is_signal?/1 function

--- 2015/day_07/gates/lib/gates/finder.ex
+++ 2015/day_07/gates/lib/gates/finder.ex
@@ -4,11 +4,9 @@ defmodule Gates.Finder do
   # Public Functions #
   ####################
 
-  def find(instructions, :signals),
-    do: instructions |> Enum.filter(&is_signal?/1)
+  def find(instructions, :signals), do: instructions |> Enum.filter(&is_signal?/1)
 
   def find([], _, _), do: :no_instructions
-
   def find(instructions, wire, known_wires) do
     known_wires = known_wires |> Enum.map(fn {wire, _} -> wire end)
     instructions |> Enum.filter(&single_node_and_pairs(&1, wire, known_wires))
@@ -18,13 +16,9 @@ defmodule Gates.Finder do
   # Private Functions #
   #####################
 
-  defp is_signal?(instruction) do
-    case instruction do
-      %{input: {signal, nil}} when is_integer(signal) -> true
-      %{input: {signal_1, signal_2}} when is_integer(signal_1) and is_integer(signal_2) -> true
-      _ -> false
-    end
-  end
+  defp is_signal?(%{input: {signal, nil}}) when is_integer(signal), do: true
+  defp is_signal?(%{input: {sig_1, sig_2}}) when is_integer(sig_1) and is_integer(sig_2), do: true
+  defp is_signal?(_), do: false
 
   defp single_node_and_pairs(%{input: {wire, other_wire}}, wire, known_wires), do:
     other_wire == nil || is_integer(other_wire) || other_wire in known_wires
