9dfd47b0dbfddfde6b40ddc2ed0ac8128ff087b9
Maciej Borowy <contact@macborowy.com>
2017-06-11 16:04:46 +0200

    initial day_08 commit

---
+++ 2015/matchsticks/lib/matchsticks.ex
@@ -0,0 +1,18 @@
+defmodule Matchsticks do
+  @moduledoc """
+  Documentation for Matchsticks.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Matchsticks.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2015/matchsticks/test/matchsticks_test.exs
@@ -0,0 +1,8 @@
+defmodule MatchsticksTest do
+  use ExUnit.Case
+  doctest Matchsticks
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
