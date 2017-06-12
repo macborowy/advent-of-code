3b2743b524c5acf6408a2c24743464f8aa701d67
Maciej Borowy <contact@macborowy.com>
2017-06-12 20:39:32 +0200

    solution to 2nd part of day 8

--- 2015/day_08/matchsticks/lib/matchsticks.ex
+++ 2015/day_08/matchsticks/lib/matchsticks.ex
@@ -1,18 +1,22 @@
 defmodule Matchsticks do
   @moduledoc false
 
-  def count_for(input) do
-    input
-    |> File.read!
-    |> String.split("\n")
-    |> Enum.reduce({0, 0}, &count/2)
-  end
+  def count_for(input, :decode), do: input |> read_file |> Enum.reduce({0, 0}, &decode/2)
+  def count_for(input, :encode), do: input |> read_file |> Enum.reduce({0, 0}, &encode/2)
 
-  def count(input, result \\ {0, 0})
-  def count(<<>>, result), do: result
-  def count(<<"\"", rest::binary>>, {code, char}), do: count(rest, {code + 1, char})
-  def count(<<"\\\\", rest::binary>>, {code, char}), do: count(rest, {code + 2, char + 1})
-  def count(<<"\\\"", rest::binary>>, {code, char}), do: count(rest, {code + 2, char + 1})
-  def count(<<"\\x", hex::16, rest::binary>>, {code, char}), do: count(rest, {code + 4, char + 1})
-  def count(<<c::8, rest::binary>>, {code, char}), do: count(rest, {code + 1, char + 1})
+  def decode(input, result \\ {0, 0})
+  def decode(<<>>, result), do: result
+  def decode(<<"\"",            rest::binary>>, {code, char}), do: decode(rest, {code + 1, char})
+  def decode(<<"\\\"",          rest::binary>>, {code, char}), do: decode(rest, {code + 2, char + 1})
+  def decode(<<"\\\\",          rest::binary>>, {code, char}), do: decode(rest, {code + 2, char + 1})
+  def decode(<<"\\x", _hex::16, rest::binary>>, {code, char}), do: decode(rest, {code + 4, char + 1})
+  def decode(<<_c::8,           rest::binary>>, {code, char}), do: decode(rest, {code + 1, char + 1})
+
+  def encode(input, result \\ {0, 0})
+  def encode(<<>>, {code, char}), do: {code + 2, char}
+  def encode(<<"\"",  rest::binary>>, {code, char}), do: encode(rest, {code + 2, char + 1})
+  def encode(<<"\\",  rest::binary>>, {code, char}), do: encode(rest, {code + 2, char + 1})
+  def encode(<<_c::8, rest::binary>>, {code, char}), do: encode(rest, {code + 1, char + 1})
+
+  defp read_file(file_path), do: file_path |> File.read! |> String.trim |> String.split("\n")
 end
--- 2015/day_08/matchsticks/test/matchsticks_test.exs
+++ 2015/day_08/matchsticks/test/matchsticks_test.exs
@@ -7,39 +7,71 @@ defmodule MatchsticksTest do
     %{strings: strings, first: strings |> Enum.at(0)}
   end
 
-  describe "acceptance tests" do
+  describe "decode acceptance tests" do
     @tag :acceptance
     test "two double quotes is two characters of code, but the string has no meaningful character" do
-      assert {2, 0} == Matchsticks.count("\"\"")
+      assert {2, 0} == Matchsticks.decode("\"\"")
     end
 
     @tag :acceptance
     test "\"abc\" is 5 characters of code, but 3 meaningful characters in the string data" do
-      assert {5, 3} == Matchsticks.count("\"abc\"")
+      assert {5, 3} == Matchsticks.decode("\"abc\"")
     end
 
     @tag :acceptance
     test "\"aaa\"aaa\" is 10 characters of code, but the string has 7 meaningful characters" do
-      assert {10, 7} == Matchsticks.count("\"aaa\\\"aaa\"")
+      assert {10, 7} == Matchsticks.decode("\"aaa\\\"aaa\"")
     end
 
     @tag :acceptance
     test "\"\x27\" is 6 characters of code, but has only one meaningful character" do
-      assert {6, 1} == Matchsticks.count("\"\\x27\"")
+      assert {6, 1} == Matchsticks.decode("\"\\x27\"")
+    end
+  end
+
+  describe "encode acceptance tests" do
+    @tag :acceptance
+    test "two double quotes encodes to 6 code characters and 2 original characters" do
+      assert {6, 2} == Matchsticks.encode("\"\"")
+    end
+
+    @tag :acceptance
+    test "\"abc\" encodes to 9 code charaters and 5 original characters" do
+      assert {9, 5} == Matchsticks.encode("\"abc\"")
+    end
+
+    @tag :acceptance
+    test "\"aaa\\\"aaa\" encodes to 16 code characters and 10 original characters" do
+       assert {16, 10} == Matchsticks.encode("\"aaa\\\"aaa\"")
+    end
+
+    @tag :acceptance
+    test "\"\\x27\" encodes to 11 code characters and 6 original characters" do
+       assert {11, 6} == Matchsticks.encode("\"\\x27\"")
     end
   end
 
   describe "tests with real input" do
     test "for the first input string return correct values", %{first: first} do
-      assert {16, 7} == Matchsticks.count(first)
+      assert {16, 7} == Matchsticks.decode(first)
+    end
+
+    test "encodes first input line correctly", %{first: first} do
+      # "\xa8br\x8bjr\"" encodes to: \"\\xa8br\\x8bjr\\\"\"
+      assert {24, 16} == Matchsticks.encode(first)
+    end
+
+    test "encodes string with double backslashes correctly", %{strings: strings} do
+      string = strings |> Enum.at(3) # "daz\\zyyxddpwk" => \"daz\\\\zyyxddpwk\"
+      assert {22, 16} == Matchsticks.encode(string)
     end
   end
 
   describe "test/1" do
     test "every input has at least two character of code for two double quotes" do
-      assert {2, 0} == Matchsticks.count("\"\"")
-      assert {3, 1} == Matchsticks.count("\"1\"")
-      assert {2, 0} == Matchsticks.count(<<"\"\"">>)
+      assert {2, 0} == Matchsticks.decode("\"\"")
+      assert {3, 1} == Matchsticks.decode("\"1\"")
+      assert {2, 0} == Matchsticks.decode(<<"\"\"">>)
     end
   end
 end
